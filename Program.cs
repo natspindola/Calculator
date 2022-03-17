c11947da4c594981173f5e720998796825b0af12
natspindola <natspindola@hotmail.com>
2022-03-17 19:49:43 -0300

    divisão

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Divisao();
         }
 
         static void Soma() {
@@ -39,6 +39,21 @@ namespace Calculator
 
             float resultado = v1 - v2;
             Console.WriteLine("O resultado da subtração é " + resultado);
+            Console.ReadKey();
+        }
+
+        static void Divisao() {
+            Console.Clear(); //dotnet run
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine("O resultado da divisão é " + resultado);
             Console.ReadKey();
         }
     }
