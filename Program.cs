df2c6222093adc8af3319b2ad1b0d8bb731895d4
natspindola <natspindola@hotmail.com>
2022-03-17 19:46:53 -0300

    subtração -

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma() {
@@ -24,6 +24,22 @@ namespace Calculator
             // Console.WriteLine($"O resultado da soma é {resultado}");
             // Console.WriteLine($"O resultado da soma é {v1 + v2}");
             // Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            Console.ReadKey();
+        }
+
+        static void Subtracao() {
+            Console.Clear(); //dotnet run
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine("O resultado da subtração é " + resultado);
+            Console.ReadKey();
         }
     }
 }
